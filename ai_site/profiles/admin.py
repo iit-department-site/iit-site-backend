a91b8c916ca0d6a010523c3754370ae55ef3434e
Furcas <tenatarika@gmail.com>
2021-07-24 14:33:27 +0300

    upgrage user model: add: bio

--- ai_site/profiles/admin.py
+++ ai_site/profiles/admin.py
@@ -12,7 +12,7 @@ class UserNetAdmin(UserAdmin):
             'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
         }),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-        (_('info'), {'fields': ('phone', 'avatar')}),
+        (_('info'), {'fields': ('phone', 'avatar', 'gender')}),
     )
 
 
--- ai_site/profiles/models.py
+++ ai_site/profiles/models.py
@@ -1,13 +1,27 @@
  
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
-# env.dev 57:44
-# '''
+ 
+ 
+
+
 class UserNet(AbstractUser):
+    
+    
+    GENDER = (
+        ('male', 'male'),
+        ('female', 'female')
+    )
+    
     '''Custom user model'''
     middle_name = models.CharField(max_length=50)
     first_login = models.DateTimeField(null=True)
     phone = models.CharField(max_length=14)
     avatar = models.ImageField(upload_to = 'user/avatar/', blank=True, null=True)
+    bio = models.TextField(blank=True, null = True)
+    github = models.CharField(max_length=500, null=True, blank=True)
+    birthday = models.DateField(blank=True, null=True)
+    gender = models.CharField(max_length=6, choices=GENDER, default='male')
+    
+    
    
