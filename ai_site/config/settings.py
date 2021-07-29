bd8e77cc825242e4be3ae79cb0ede809a0ec1a11
Furcas <tenatarika@gmail.com>
2021-07-30 01:21:06 +0300

    add post in wall.models

--- ai_site/config/settings.py
+++ ai_site/config/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'corsheaders',
     
     'profiles',
+    'wall',
 ]
 
 MIDDLEWARE = [
--- ai_site/wall/models.py
+++ ai_site/wall/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+class  Post(models.Model):
+    """Post model"""
+    text = models.TextField(max_length=1000)
+    created_date = models.DateTimeField(auto_now_add=True)
+    published = models.BooleanField(default=True)
+    moderation = models.BooleanField(default=True)
+    view_count = models.PositiveIntegerField(default=0)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'Post created by {self.user}'
+    
+    
+    
