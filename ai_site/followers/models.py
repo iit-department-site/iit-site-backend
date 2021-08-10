5aa5db9512cada929b86ee4eb23a6618d914340e
Furcas <tenatarika@gmail.com>
2021-08-10 21:53:44 +0300

    add model Follower

--- ai_site/followers/models.py
+++ ai_site/followers/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
-# Create your models here.
+from django.conf import settings
+ 
+class Follower(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    subscriber = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
+    
+    
