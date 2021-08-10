d1d2b267a9423576779848a69c2ca25e29fcac1c
Furcas <tenatarika@gmail.com>
2021-08-10 22:04:27 +0300

    add serializers for Follower

---
+++ ai_site/followers/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Follower
+from profiles.serializers import UserByFollowerSerializer
+
+class ListFollowerSerializer(serializers.ModelSerializer):
+    
+    subscriber = UserByFollowerSerializer(many=True, read_only=True)
+    
+    
+    class Meta:
+        model = Follower
+        fields = ('subscriber', )
