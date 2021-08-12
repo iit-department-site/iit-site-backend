2e468cc51b938999c82c5f67b02a2d8db65437a7
Furcas <tenatarika@gmail.com>
2021-08-12 23:05:55 +0300

    update model follower

--- ai_site/followers/models.py
+++ ai_site/followers/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.conf import settings
  
 class Follower(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    subscriber = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='owner')
+    subscriber = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='followers')
     
     
     
