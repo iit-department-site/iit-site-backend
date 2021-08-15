fc11650359da2106381e554e4da904448dc40b40
Furcas <tenatarika@gmail.com>
2021-08-15 22:35:34 +0300

    add delete follower

--- ai_site/followers/views.py
+++ ai_site/followers/views.py
@@ -30,4 +30,14 @@ class AddFollowerView(views.APIView):
             return response.Response(status=201)
         return response.Response(status=404)
        
- 
+    
+    
+    def delete(self, request, pk):
+        try:
+            sub = Follower.objects.get(subscriber=request.user, user_id=pk)
+        
+        except Follower.DoesNotExist:
+            return response.Response(status= 404)
+        
+        sub.delete()
+        return response.Response(status=204)
