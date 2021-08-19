034f72425fd2bee262cb4054f9da3258399dd926
Furcas <tenatarika@gmail.com>
2021-08-19 23:18:58 +0300

    create user

--- ai_site/feed/management/commands/addfake.py
+++ ai_site/feed/management/commands/addfake.py
@@ -8,3 +8,17 @@ class Command(BaseCommand):
     
     def handle(self, *args, **kwargs):
         pass
+    
+    def create_user(self):
+        for i in range(10):
+            user = UserNet.objects.create(
+                username=f"test {i}",
+                email=f'test{i}@gmail.com',
+                is_active = True,
+                middle_name =f"test{i}",
+                phone=f'12345678910{i}',
+                gender = 1
+            )
+            
+            user.set_password('test')
+            user.save()
