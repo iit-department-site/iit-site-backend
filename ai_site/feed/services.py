81066fe023e9dab7f4bb81f5b2f1a19260444478
Furcas <tenatarika@gmail.com>
2021-08-24 23:58:41 +0300

    fix services

--- ai_site/feed/services.py
+++ ai_site/feed/services.py
@@ -4,7 +4,7 @@ from wall.models import Post
 class Feed:
         
     def get_post_list(self, user): 
-        return posts = Post.objects.filter(user__owner__subscriber=user).order_by('-created_date')\
+        return  Post.objects.filter(user__owner__subscriber=user).order_by('-created_date')\
             .select_related('user').prefetch_related('comments')
             
             
