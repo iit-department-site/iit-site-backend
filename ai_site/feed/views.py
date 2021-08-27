bede72245bd6d27a2255b5a548fe1c96c6984b5f
Furcas <tenatarika@gmail.com>
2021-08-27 23:57:58 +0300

    retrieve feed

--- ai_site/feed/views.py
+++ ai_site/feed/views.py
@@ -1,23 +1,27 @@
 
 
-from django.db.models.query import QuerySet
 
-from rest_framework import generics, permissions, viewsets, response
-from rest_framework.serializers import Serializer
-from wall.serializers import ListPostSerializer
-from base.classes import MixedPermission
+from rest_framework import permissions, viewsets, response
+
+from wall.serializers import ListPostSerializer, PostSerilizer
+
 
 from .services import feed_service
 
 
 
 class FeedView( viewsets.GenericViewSet):
+    
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = ListPostSerializer
-    
+
     def list(self, request, *args, **kwargs):
-        
         queryset = feed_service.get_post_list(request.user)
-        serializer = self.get_serializer(queryset, many = True)
+        serializer = self.get_serializer(queryset, many=True)
+        return response.Response(serializer.data)
+    
+    def retrieve(self, request, *args, **kwargs):
+        instance = feed_service.get_single_post(kwargs.get('pk'))
+        serializer = PostSerilizer(instance)
         return response.Response(serializer.data)
         
